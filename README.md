b3699dd7e1d78826f00c700eeaed5b4d41533f05
sojebsikder <sojebsikder@gmail.com>
2021-06-22 22:05:54 +0600

    main

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Blog app created with python django
 
 ## Packages
+pip install mysqlclient-1.4.6-cp38-cp38-win32.whl
 pip install django-crispy-forms
--- blogsite/settings.py
+++ blogsite/settings.py
@@ -82,8 +82,16 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
